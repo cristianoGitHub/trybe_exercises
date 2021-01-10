76a36e5b347d432d208c2b8c38b8ce78c337098c
Cristiano Roberto <cristianoroberto@outlook.com>
2021-01-10 09:32:24 -0300

    Subindo os arquivos de exercícios

---
+++ introducao-a-javascript-e-logica-de-programacao_3/exercicio_1.js
@@ -0,0 +1,11 @@
+let n = 5;
+let linhaIndex;
+let astericos = '*';
+let insereLinha = '';
+
+for (linhaIndex = 0; linhaIndex < n; linhaIndex += 1) {
+  insereLinha = insereLinha + astericos;
+};
+for (linhaIndex = 0; linhaIndex < n; linhaIndex += 1) {
+  console.log(insereLinha);
+};
